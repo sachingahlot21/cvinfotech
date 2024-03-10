aa9bb82d3c3d97d76e24b56a9cdcda86fb9b5944
sachingahlot21 <gahlotsachin69@gmail.com>
2024-03-10 22:05:48 +0530

    cv info

---
+++ netflix/script.js
@@ -0,0 +1,41 @@
+
+// <
+const btnnxt = document.getElementById('btndiv1')
+        btnnxt.style.display = 'none'
+
+function handlePrevBtn(){
+    let ul = document.getElementById('div2ul')
+    const style = ul.currentStyle || window.getComputedStyle(ul)
+    const currentMargin =  parseInt(style.marginLeft)
+    if(currentMargin < 0 && currentMargin > -1260){
+        ul.style.marginLeft = `${currentMargin+860}px`
+        const btnnxt = document.getElementById('btndiv1')
+        btnnxt.style.display = 'none'
+        const btnnxt2 = document.getElementById('btndiv2')
+        btnnxt2.style.display = 'block'
+    }
+    else if(currentMargin <= -1260){
+        ul.style.marginLeft = `${currentMargin+400}px`
+        const btnnxt2 = document.getElementById('btndiv2')
+        btnnxt2.style.display = 'block'
+       
+    }
+}
+
+// >
+function handleNextBtn(){
+    let ul = document.getElementById('div2ul')
+    const style = ul.currentStyle || window.getComputedStyle(ul)
+    const currentMargin =  parseInt(style.marginLeft)
+    if(currentMargin > -800){
+        ul.style.marginLeft = `${currentMargin-860}px`
+        const btnnxt = document.getElementById('btndiv1')
+        btnnxt.style.display = 'block'
+    }
+    else if(currentMargin < -800){
+        ul.style.marginLeft = `${currentMargin-400}px`
+       
+        const btnnxt = document.getElementById('btndiv2')
+        btnnxt.style.display = 'none'
+    }
+} 
